7344e00b595493a3b7a4c24b0fc7c0f0fb206ae3
Terence <tserence@gmail.com>
2020-09-05 12:55:19 -0700

    add primitive cache

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import jsonify
 import json
 from savingsAccounts import SavingsAccountDAO
+from cache import Cache
 from google.cloud import datastore
 
 app = Flask(__name__)
@@ -10,13 +11,22 @@ db = datastore.Client().from_service_account_json('../what-apy.json')
 kind = 'flat_apy'
 name = 'savings_acc_name'
 key = db.key(kind, name)
+cache = Cache()
+
+savings_accounts_api_key = 'savings_accounts'
 
 @app.route('/savings_accounts')
 def savings_accounts():
-  query = db.query(kind=kind)
-  savings_accs = query.fetch()
-  savingsDao = SavingsAccountDAO()
-  accs_by_bank = savingsDao.groupByBank(savings_accs)
+  accs_by_bank = cache.get(savings_accounts_api_key)
+
+  if accs_by_bank: print('cache hit')
+
+  if not accs_by_bank:
+    query = db.query(kind=kind)
+    savings_accs = query.fetch()
+    savingsDao = SavingsAccountDAO()
+    accs_by_bank = savingsDao.groupByBank(savings_accs)
+    cache.put(savings_accounts_api_key, accs_by_bank)
 
   return jsonify([acc.serialise() for banks, types in accs_by_bank.items() for t, acc in types.items()])
 
---
+++ server/cache.py
@@ -0,0 +1,10 @@
+class Cache():
+  def __init__(self):
+    self.cache = {}
+
+  def put(self, key, value):
+    self.cache[key] = value
+
+  def get(self, key):
+    return None if key not in self.cache \
+      else self.cache[key]
